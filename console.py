a5f041d99db451a5e45bc412a26adf064134a70f
luissantananet <luissantananet@gmail.com>
2021-11-09 09:19:28 -0300

    reformulado o cod fonte e refeito a conexões DB

--- console.py
+++ console.py
@@ -8,8 +8,8 @@ numero_id = 0
 banco = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="Cofggcvf",
-    database="newprojet"
+    passwd="cofggcvf",
+    database="dbstore"
 )
 # fuções da tela login
 def funcao_login():
@@ -18,7 +18,7 @@ def funcao_login():
     key = frm_login.linekey.text()
     
     cursor2 = banco.cursor()
-    comando_sql = "SELECT senha FROM login WHERE nome ='{}'".format(nome_user)
+    comando_sql = "SELECT senha FROM tbllogin WHERE login ='{}'".format(nome_user)
     cursor2.execute(comando_sql)
     #nome = cursor2.fetchall()
     senha_db = cursor2.fetchall()
@@ -60,14 +60,16 @@ def cadastroProduto():
         banco.commit()
 # fuções da tela de cadastro Usuário
 def cadastrousuario():
+    linhanome = frm_caduser.lineNome.text() #campo Nome
     linhauser = frm_caduser.lineUser.text() #campo Usuário
     linhakey = frm_caduser.lineKey.text() #campo senha
     #comando mysql para inserir dados no banco
     cursor = banco.cursor()
-    comando_SQL = "INSERT INTO login (nome, senha) VALUES (%s,%s)"
-    dados = (str(linhauser),str(linhakey))
+    comando_SQL = "INSERT INTO tbllogin (nome, login, senha) VALUES (%s,%s,%s)"
+    dados = (str(linhanome),str(linhauser),str(linhakey))
     cursor.execute(comando_SQL,dados)
     banco.commit()
+    frm_caduser.lineNome.setText('')
     frm_caduser.lineUser.setText('')
     frm_caduser.lineKey.setText('')
 # fuções da tela da Calcular Markup
@@ -91,13 +93,14 @@ def enviarmarkup():
 def funcao_cancela():
     frm_login.close()
 
-def funcao_cadastro():
+def chamacliente():
+    frm_cliente.show()
+def chamaestoque():
+    frm_estoque.show()
+
+def chamaproduto():
     frm_produto.show()
-    id = frm_produto.lineID.text()
-    if id == "":
-        id = 0
-        ids =int(id)+1
-        frm_produto.lineID.setText(ids)
+   
 def chamamarkup():
     frm_markup.show()
 
@@ -111,37 +114,37 @@ def fechaVendas():
     frm_vendas.close()
 
 def Chamapesquisa():
-    frm_pesquisa.show()
+    frm_pesquisa_produto.show()
     cursor = banco.cursor()
-    comando_SQL = "SELECT * FROM produtos"
+    comando_SQL = "SELECT * FROM tblproduto"
     cursor.execute(comando_SQL)
     dados_lidos = cursor.fetchall()
-    frm_pesquisa.tableWidget.setRowCount(len(dados_lidos))
-    frm_pesquisa.tableWidget.setColumnCount(8)
+    frm_pesquisa_produto.tableWidget.setRowCount(len(dados_lidos))
+    frm_pesquisa_produto.tableWidget.setColumnCount(8)
     for i in range(0, len(dados_lidos)):
         for j in range(0, 5):
-            frm_pesquisa.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
+            frm_pesquisa_produto.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
 
 def exclir_produtos():
-    linha = frm_pesquisa.tableWidget.currentRow()
-    frm_pesquisa.tableWidget.removeRow(linha)
+    linha = frm_pesquisa_produto.tableWidget.currentRow()
+    frm_pesquisa_produto.tableWidget.removeRow(linha)
 
     cursor = banco.cursor()
-    cursor.execute("SELECT id FROM produtos")
+    cursor.execute("SELECT id FROM tblproduto")
     dados_lidos = cursor.fetchall()
     valor_id = dados_lidos[linha][0]
     cursor.execute("DELETE FROM produtos WHERE id="+str(valor_id))
 
 def editar_produto():
     global numero_id
-    linha = frm_pesquisa.tableWidget.currentRow()
+    linha = frm_pesquisa_produto.tableWidget.currentRow()
 
     cursor = banco.cursor()
-    comando_SQL = "SELECT * FROM produtos"
+    comando_SQL = "SELECT * FROM tblproduto"
     cursor.execute(comando_SQL)
     dados_lidos = cursor.fetchall()
     valor_id = dados_lidos[linha][0]
-    cursor.execute("SELECT * FROM produtos WHERE id"+str(valor_id))
+    cursor.execute("SELECT * FROM tblproduto WHERE id"+str(valor_id))
     produto = cursor.fetchall()
     frm_produto.show()
 
@@ -164,36 +167,42 @@ def cadastroCliente():
 if __name__ == "__main__":
     # chamando as telas
     app = QtWidgets.QApplication([])
-    frm_login = uic.loadUi(r'.\NovoProjeto\forms\frm_login.ui')
-    frm_principal = uic.loadUi(r'.\NovoProjeto\forms\frm_principal.ui')
-    frm_vendas = uic.loadUi(r'.\NovoProjeto\forms\frm_vendas.ui')
-    frm_produto = uic.loadUi(r'.\NovoProjeto\forms\frm_produto.ui')
-    frm_pesquisa = uic.loadUi(r'.\NovoProjeto\forms\frm_pesquisa_v2.ui')
-    frm_caduser = uic.loadUi(r'.\NovoProjeto\forms\frm_caduser.ui')
-    frm_markup = uic.loadUi(r'.\NovoProjeto\forms\frm_markup.ui')
+    frm_login = uic.loadUi(r'.\forms\frm_login.ui')
+    frm_principal = uic.loadUi(r'.\forms\frm_principal.ui')
+    frm_vendas = uic.loadUi(r'.\forms\frm_vendas.ui')
+    frm_produto = uic.loadUi(r'.\forms\frm_produto.ui')
+    frm_pesquisa_produto = uic.loadUi(r'.\forms\frm_pesquisa_produtos.ui')
+    frm_caduser = uic.loadUi(r'.\forms\frm_caduser.ui')
+    frm_markup = uic.loadUi(r'.\forms\frm_markup.ui')
+    frm_cliente = uic.loadUi(r'.\forms\frm_cadastro_Cliente.ui')
+    frm_estoque= uic.loadUi(r'.\forms\frm_estoque.ui')
     # botões da tela login
     frm_login.linekey.setEchoMode(QtWidgets.QLineEdit.Password)
     frm_login.btnlogin.clicked.connect(funcao_login)
     frm_login.btnexit.clicked.connect(funcao_cancela)
     # botões da tela principal
-    frm_principal.actionProdutos.triggered.connect(funcao_cadastro)
+    frm_principal.actionProdutos.triggered.connect(chamaproduto)
     frm_principal.actionCaixa.triggered.connect(ChamaVenda)
     frm_principal.actionUser.triggered.connect(chamacaduser)
+    frm_principal.actionClientes.triggered.connect(chamacliente)
+    frm_principal.actionEstoque.triggered.connect(chamaestoque)
+
     # botões da tela venda(caixa)
-    frm_vendas.btnpesquisaprod_2.clicked.connect(Chamapesquisa)
-    frm_vendas.btnfechar.clicked.connect(fechaVendas)
+    frm_vendas.btn_pesquisaprod.clicked.connect(Chamapesquisa)
+    frm_vendas.btn_fechar.clicked.connect(fechaVendas)
     # botões da tela cadastro de produtos
     frm_produto.btnSalvar.clicked.connect(cadastroProduto)
     frm_produto.btnPesquisa.clicked.connect(Chamapesquisa)
-    frm_produto.btnPesquisamarkup.clicked.connect(chamamarkup)
+    frm_produto.btn_calc_markup.clicked.connect(chamamarkup)
     # botões da tela markup
     frm_markup.btncalcular.clicked.connect(calcular)
     frm_markup.btnenviar.clicked.connect(enviarmarkup)    
     # botões da tela cadastro de Usuáios
     frm_caduser.btnSalvar.clicked.connect(cadastrousuario)
     # botões da tela de pesquisa
-    frm_pesquisa.btnExcluir.clicked.connect(exclir_produtos)
-    frm_pesquisa.btnEdit.clicked.connect(editar_produto)
+    frm_pesquisa_produto.btnExcluir.clicked.connect(exclir_produtos)
+    frm_pesquisa_produto.btnEdit.clicked.connect(editar_produto)
+    frm_pesquisa_produto.btn_estoque.clicked.connect(chamaestoque)
 
     frm_login.show()
     app.exec()
